09876b297d455296c97d16f3b89b291208abb51f
Dinda Maharani <143940529+Dindamaharn@users.noreply.github.com>
2025-06-09 08:03:47 +0700

    paket user add link awesome

--- user/packages.php
+++ user/packages.php
@@ -23,6 +23,7 @@ $result = mysqli_query($conn, $query);
   <meta charset="UTF-8">
   <title>Daftar Paket Travel</title>
   <link rel="stylesheet" href="../css/user/packages.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
       <meta charset="UTF-8" />
     <title>Kiran Travel & Tour</title>
 </head>
