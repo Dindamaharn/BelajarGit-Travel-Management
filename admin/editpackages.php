c1e150f52d9d77163c8808b4c02221c27cc1ec34
Dinda Maharani <143940529+Dindamaharn@users.noreply.github.com>
2025-08-12 22:08:50 +0700

    test konflik 2

--- admin/editpackages.php
+++ admin/editpackages.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($stmt->execute()) {
             $_SESSION['alert'] = [
                 'type' => 'success',
-                'message' => 'Paket berhasil diperbarui.'
+                'message' => 'Paket berhasil diperbarui, anjay gurinjay'
             ];
             header("Location: managepackages.php");
             exit();
